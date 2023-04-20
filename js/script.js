d57a90934956e3e0daf48245d190a7f613c66eed
james <jkuyt39@gmail.com>
2023-04-20 22:20:44 +0000

    meow

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
-const button = document.getElementById("btn");
-button.addEventListener("click", function() {
+const catButton = document.getElementById("btn");
+catButton.addEventListener("click", function() {
   window.location.href = "https://catchat-meow.netlify.app/";
 });
 
-  const button = document.getElementById("link");
-  button.addEventListener("click", function() {
-    window.location.href = "https://teslakitty.itch.io";
-  });
-
+const itchButton = document.getElementById("link");
+itchButton.addEventListener("click", function() {
+  window.location.href = "https://teslakitty.itch.io";
+});
