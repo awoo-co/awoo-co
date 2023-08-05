48a7f44dce0a8ae151bb4d388f824677c23bbc4d
James <jkuyt39@gmail.com>
2023-08-05 18:03:37 +0000

    meow

--- js/script.js
+++ js/script.js
@@ -1,14 +1,34 @@
 const catButton = document.getElementById("btn");
 catButton.addEventListener("click", function() {
-    window.location.href = "https://catchat-meow.netlify.app/";
+  window.location.href = "https://catchat-meow.netlify.app/";
 });
 
 const itchButton = document.getElementById("btn2");
 itchButton.addEventListener("click", function() {
-    window.location.href = "https://teslakitty.itch.io";
+  window.location.href = "https://teslakitty.itch.io";
 });
 
 const btn3 = document.getElementById("btn3");
 btn3.addEventListener("click", function() {
-    window.location.href = "https://cookie2.netlify.app/";
+  window.location.href = "https://cookie2.netlify.app/";
+});
+
+const btn4 = document.getElementById("btn4");
+btn4.addEventListener("click", function() {
+  window.location.href = "https://catchat-meow.netlify.app/"; 
+});
+
+const btn5 = document.getElementById("btn5");
+btn5.addEventListener("click", function() {
+  window.location.href = "https://cattycat.netlify.app/"; 
+});
+
+const btn6 = document.getElementById("btn6");
+btn6.addEventListener("click", function() {
+  window.location.href = "https://chatgpt-skill.netlify.app/"; 
+});
+
+const btn7 = document.getElementById("btn7");
+btn7.addEventListener("click", function() {
+  window.location.href = "https://new-video.netlify.app/"; 
 });
