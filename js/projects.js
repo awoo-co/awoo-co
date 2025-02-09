731b39135a3b07c8fc7051a7b8bc4629de88021f
teslakitty1 <jameskuyt655@gmail.com>
2025-02-09 07:22:55 -0500

    Refactor 404 error handling and improve project page layout

--- js/projects.js
+++ js/projects.js
@@ -1,44 +1,4 @@
-const catButton = document.getElementById("btn");
-catButton.addEventListener("click", function() {
-  window.location.href = "https://catchat-meow.netlify.app/";
-});
-
-const itchButton = document.getElementById("btn2");
-itchButton.addEventListener("click", function() {
-  window.location.href = "https://teslakitty.itch.io";
-});
-
-const btn3 = document.getElementById("btn3");
-btn3.addEventListener("click", function() {
-  window.location.href = "https://cookie2.netlify.app/";
-});
-
-const btn5 = document.getElementById("btn5");
-btn5.addEventListener("click", function() {
-  window.location.href = "https://cattycat.netlify.app/"; 
-});
-
-const btn6 = document.getElementById("btn6");
-btn6.addEventListener("click", function() {
-  window.location.href = "https://chatgpt-skill.netlify.app/"; 
-});
-
-const btn7 = document.getElementById("btn7");
-btn7.addEventListener("click", function() {
-  window.location.href = "https://new-video.netlify.app/"; 
-});
-
-const btn8 = document.getElementById("btn8");
-btn8.addEventListener("click", function() {
-  window.location.href = "https://teslakitty-cdn.netlify.app/talk_to_me.apk";
-});
-
-const btn9 = document.getElementById("btn9");
-btn9.addEventListener("click", function() {
- alert('this file is now removed because of a virus from the apk')
-});
-
-const btn10 = document.getElementById("btn10");
-btn10.addEventListener("click", function() {
-  window.location.href = "https://github.com/teslakitty";
+// Add an event listener to the button to open the URL in a new tab
+btn1.addEventListener("click", function() {
+  window.open("https://github.com/orgs/teslakitty/repositories", "_blank");
 });
