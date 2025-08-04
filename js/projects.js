fe677bb80ed403a725b9ab1496cbb22e8b674d32
Jam <jameskuyt655@gmail.com>
2025-08-04 18:33:31 +0000

    updated the company name in error.html and fixed broken link that takes you to a broken 404 github page of the old company name

--- js/projects.js
+++ js/projects.js
@@ -1,4 +1,4 @@
 // Add an event listener to the button to open the URL in a new tab
 btn1.addEventListener("click", function() {
-  window.open("https://github.com/orgs/teslakitty/repositories", "_blank");
+  window.open("https://github.com/orgs/awoo-co/repositories", "_blank");
 });
