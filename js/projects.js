84d670061a5f15fa374cbb861bb306c4f0630789
Jamjam7665 <jameskuyt655@gmail.com>
2026-02-24 17:42:43 -0500

    Update projects.js

--- js/projects.js
+++ js/projects.js
@@ -1,5 +1,5 @@
 // Add an event listener to the button to open the URL in a new tab
-const btn1 = document.getElementById("btn1"); // ensure your button has id="btn1"
+const btn1 = document.getElementById("btn1");
 
 if (btn1) {
   btn1.addEventListener("click", () => {
