25fa71d0874a8cfb279a11e13d2b034c5c985b4a
Jamjam7665 <jameskuyt655@gmail.com>
2026-02-24 17:42:15 -0500

    Fixing button from not working

--- js/projects.js
+++ js/projects.js
@@ -1,4 +1,8 @@
 // Add an event listener to the button to open the URL in a new tab
-btn1.addEventListener("click", function() {
-  window.open("https://github.com/orgs/awoo-co/repositories", "_blank");
-});
+const btn1 = document.getElementById("btn1"); // ensure your button has id="btn1"
+
+if (btn1) {
+  btn1.addEventListener("click", () => {
+    window.open("https://github.com/orgs/awoo-co/repositories", "_blank", "noopener,noreferrer");
+  });
+}
